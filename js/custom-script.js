05e29d7cd096fe9732354dda390db39c57e40b43
Gunal7Devloper <114910887+Gunal7Devloper@users.noreply.github.com>
2025-06-15 16:16:25 +0000

    html2canvas plugin added

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,5 +1,13 @@
 function takeSnapshot() {
-    alert("Snapshot taken!");
     // Here you can add the logic to take a snapshot
-    
+    const div = document.getElementById("capture-template");
+    html2canvas(div).then(canvas => {
+        // Create image from canvas
+        const imgData = canvas.toDataURL("image/png");
+        // Optional: Download the image
+        const link = document.createElement("a");
+        link.href = imgData;
+        link.download = "screenshot.png";
+        link.click();
+    });
 }
